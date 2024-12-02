0955871e25544bd38e8860830ab9be064690709c
Kaleb Elwert <belak@coded.io>
2024-12-01 23:13:50 -0800

    Support overriding the protos path

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("cargo:rerun-if-changed=migrations");
 
+    let protos_path = std::env::var("SEABIRD_PROTO_PATH").unwrap_or_else(|_| "./proto".to_string());
+
     // NOTE: we need to use configure rather than the simple case because both
     // seabird and seabird_chat_ingest use the same package name. This also gives
     // us the option to disable client generation so we do that.
@@ -8,8 +10,8 @@ fn main() {
         .build_client(false)
         .build_server(true)
         .compile(
-            &["./proto/seabird.proto", "./proto/seabird_chat_ingest.proto"],
-            &["./proto"],
+            &["seabird.proto", "seabird_chat_ingest.proto"],
+            &[&protos_path],
         )
         .unwrap();
 }
